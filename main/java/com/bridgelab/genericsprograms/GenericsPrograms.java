ae5fa237bbaabc51334e8fa78fb4b79e8ff50298
navyas211@gmail.com <navyas211@gmail.com>
2021-10-04 23:03:52 +0530

    [NAVYA]ADDED: Welcome To Generics Programs

---
+++ main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
@@ -0,0 +1,8 @@
+package com.bridgelab.genericsprograms;
+
+public class GenericsPrograms {   
+        public static void main(String[] args) {
+			System.out.println("Welcome to Generics Programs");
+		}
+
+}
---
+++ test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
@@ -0,0 +1,9 @@
+package com.bridgelab.genericsprogram.test;
+
+public class TestGenericsPrograms {  
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Testing in Generics Programs");
+	}
+
+}
