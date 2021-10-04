c07b620f40ec94be4fafe9813e0b253f6d056542
navyas211@gmail.com <navyas211@gmail.com>
2021-10-05 00:21:42 +0530

    [NAVYA]ADDED: Given 3 Floats find the maximum

--- main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
+++ main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
@@ -1,14 +1,16 @@
 package com.bridgelab.genericsprograms;
 
 public class GenericsPrograms {
-	public static Integer findingMaxNumber(Integer num1, Integer num2, Integer num3) {
-		int max = num1;
-		if (num2.compareTo(max) > 0) {
-			max = num2;
-		}
-		if (num3.compareTo(max) > 0) {
-			max = num3;
-		}
-		return max;
-	}
+	
+	// Now I Solve UC2- Given 3 Floats find the Maximum........ 
+	 public static Float findingMaxFloatNumber(Float num1, Float num2, Float num3) {
+         Float max = num1;
+         if (num2.compareTo(max) > 0) {
+             max = num2;
+         }
+         if (num3.compareTo(max) > 0) {
+             max = num3;
+         }
+         return max;
+     }
 }
--- test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
+++ test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
@@ -6,28 +6,31 @@ import org.junit.Test;
 import com.bridgelab.genericsprograms.GenericsPrograms;
 
 public class TestGenericsPrograms {
+
+	// Now I Solve UC2- Given 3 Float find the Maximum........
+
 	@Test
-	public void givenThreeNumber_AsInter_Should_ReturnMaxAmoungThem() {
-		Integer maxNumber = GenericsPrograms.findingMaxNumber(15, 88, 45);
-		Assert.assertEquals(Integer.valueOf(88), maxNumber);
+	public void givenThreeNumber_AsFloat_Should_ReturnMaxAmoungThem() {
+		Float maxNumber = GenericsPrograms.findingMaxFloatNumber(10.2f, 45.3f, 8.4f);
+		Assert.assertEquals(Float.valueOf(45.3f), maxNumber);
 	}
 
 	@Test
-	public void givenMaxNumberAt_1stPosition_Should_ReturnMax() {
-		Integer maxNumber = GenericsPrograms.findingMaxNumber(50, 40, 30);
-		Assert.assertEquals(Integer.valueOf(50), maxNumber);
+	public void givenMaxNumberAt_1stPosition_Should_Return_SameNumber() {
+		Float maxNumber = GenericsPrograms.findingMaxFloatNumber(50.2f, 40.2f, 30.2f);
+		Assert.assertEquals(Float.valueOf(50.2f), maxNumber);
 	}
 
 	@Test
 	public void givenMaxNumberAt_2ndPosition_Should_Return_SameNumber() {
-		Integer maxNumber = GenericsPrograms.findingMaxNumber(40, 50, 30);
-		Assert.assertEquals(Integer.valueOf(50), maxNumber);
+		Float maxNumber = GenericsPrograms.findingMaxFloatNumber(40.3f, 50.2f, 30.3f);
+		Assert.assertEquals(Float.valueOf(50.2f), maxNumber);
 	}
 
 	@Test
 	public void givenMaxNumberAt_3rdPosition_Should_Return_SameNumber() {
-		Integer maxNumber = GenericsPrograms.findingMaxNumber(40, 30, 50);
-		Assert.assertEquals(Integer.valueOf(50), maxNumber);
+		Float maxNumber = GenericsPrograms.findingMaxFloatNumber(40.4f, 30.4f, 50.2f);
+		Assert.assertEquals(Float.valueOf(50.2f), maxNumber);
 	}
 
 }
