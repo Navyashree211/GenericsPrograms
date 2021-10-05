a8b538e0aca25b1e84317a8a3565ddf70e93ee78
navyas211@gmail.com <navyas211@gmail.com>
2021-10-05 22:05:08 +0530

    [NAVYA]ADDED: Given 3 String find the maximum

--- main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
+++ main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
@@ -1,8 +1,17 @@
 package com.bridgelab.genericsprograms;
 
-public class GenericsPrograms {   
-        public static void main(String[] args) {
-			System.out.println("Welcome to Generics Programs");
-		}
+public class GenericsPrograms {
 
+	 Now I Solve UC2- Given 3 Strings find the Maximum........
+
+	public static String findingMaxString(String string, String string2, String string3) {
+		String max = string;
+		if (string2.compareTo(max) > 0) {
+			max = string2;
+		}
+		if (string3.compareTo(max) > 0) {
+			max = string3;
+		}
+		return max;
+	}
 }
--- test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
+++ test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
@@ -1,9 +1,33 @@
 package com.bridgelab.genericsprogram.test;
 
-public class TestGenericsPrograms {  
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Testing in Generics Programs");
-	}
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelab.genericsprograms.GenericsPrograms;
+
+public class TestGenericsPrograms {   
+	// Now I Solve UC3- Given 3 String find the Maximum........   
+    @Test
+    public void givenThreeString_Should_ReturnMaxAmoungThem()  {
+        String maxString = GenericsPrograms.findingMaxString("Apple", "Banana", "PineApple");
+        Assert.assertEquals("PineApple", maxString);
+    }
+
+    @Test
+    public void givenMaxNumberAt_1stPosition_Should_Return_SameString() {
+        String maxString = GenericsPrograms.findingMaxString("L", "Z", "M");
+        Assert.assertEquals("Z", maxString);
+    }
+	@Test
+    public void givenMaxNumberAt_2ndPosition_Should_Return_SameString() {
+        String maxString = GenericsPrograms.findingMaxString("L", "Z", "M");
+        Assert.assertEquals("Z", maxString);
+    }
+    @Test
+    public void givenMaxNumberAt_3rdPosition_Should_Return_SameString() {
+        String maxString = GenericsPrograms.findingMaxString("L", "Z", "M");
+        Assert.assertEquals("Z", maxString);
+    }
+
 
 }
