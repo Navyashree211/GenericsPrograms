024dd53ec418011fade55f1bdf011db15763b205
navyas211@gmail.com <navyas211@gmail.com>
2021-10-07 01:17:23 +0530

    [NAVYA]ADDED: Refactor all the 3 one Generic Methods and find the maximum

--- main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
+++ main/java/com/bridgelab/genericsprograms/GenericsPrograms.java
@@ -1,17 +1,38 @@
 package com.bridgelab.genericsprograms;
 
-public class GenericsPrograms {
-
-	 Now I Solve UC2- Given 3 Strings find the Maximum........
-
-	public static String findingMaxString(String string, String string2, String string3) {
-		String max = string;
-		if (string2.compareTo(max) > 0) {
-			max = string2;
-		}
-		if (string3.compareTo(max) > 0) {
-			max = string3;
-		}
+public class GenericsPrograms<E> {
+	public static <X extends Comparable> X findingMaxGenerics(X a1, X a2, X a3) {
+		X max = a1;
+		if (a1.hashCode() > a2.hashCode() && a1.hashCode() > a3.hashCode()) {
+			max = a1;
+		} else if (a2.hashCode() > a1.hashCode() && a2.hashCode() > a3.hashCode()) {
+			max = a2;
+		} else
+			max = a3;
 		return max;
 	}
+
+	public static <X> X testMaximum(X x, X y, X z) {
+		X max = x;
+		if (x.hashCode() > y.hashCode() && x.hashCode() > z.hashCode()) {
+			max = x;
+		} else if (y.hashCode() > x.hashCode() && y.hashCode() > z.hashCode()) {
+			max = y;
+		} else
+			max = z;
+		return max;
+
+	}
+
+	public static <X> void printMaxNumber(X maxNumberToBePrinted) {
+		System.out.println("Maximum number among three number:" + maxNumberToBePrinted);
+	}
+
+	public <X> X testMaximum() {
+
+		X maxNumber = (X) testMaximum();
+		printMaxNumber(maxNumber);
+		return maxNumber;
+	}
+
 }
--- test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
+++ test/java/com/bridgelab/genericsprogram/test/TestGenericsPrograms.java
@@ -5,29 +5,42 @@ import org.junit.Test;
 
 import com.bridgelab.genericsprograms.GenericsPrograms;
 
-public class TestGenericsPrograms {   
-	// Now I Solve UC3- Given 3 String find the Maximum........   
-    @Test
-    public void givenThreeString_Should_ReturnMaxAmoungThem()  {
-        String maxString = GenericsPrograms.findingMaxString("Apple", "Banana", "PineApple");
-        Assert.assertEquals("PineApple", maxString);
-    }
+public class TestGenericsPrograms {
 
-    @Test
-    public void givenMaxNumberAt_1stPosition_Should_Return_SameString() {
-        String maxString = GenericsPrograms.findingMaxString("L", "Z", "M");
-        Assert.assertEquals("Z", maxString);
-    }
 	@Test
-    public void givenMaxNumberAt_2ndPosition_Should_Return_SameString() {
-        String maxString = GenericsPrograms.findingMaxString("L", "Z", "M");
-        Assert.assertEquals("Z", maxString);
-    }
-    @Test
-    public void givenMaxNumberAt_3rdPosition_Should_Return_SameString() {
-        String maxString = GenericsPrograms.findingMaxString("L", "Z", "M");
-        Assert.assertEquals("Z", maxString);
-    }
+	public void givenThreeNumber_AsInteger_Should_ReturnMaxAmoungThem() {
+		Integer maxNumber = GenericsPrograms.testMaximum(20, 89, 45);
+		Assert.assertEquals(Integer.valueOf(89), maxNumber);
+	}
 
+	@Test
+	public void givenThreeNumber_AsFloat_Should_ReturnMaxAmoungThem() {
+		Float maxNumber = GenericsPrograms.testMaximum(10.2f, 45.3f, 8.4f);
+		Assert.assertEquals(Float.valueOf(45.3f), maxNumber);
+	}
+
+	@Test
+	public void givenThreeValues_Should_ReturnMaxAmongThem() {
+		String maxString = GenericsPrograms.testMaximum("L", "Z", "M");
+		Assert.assertEquals("Z", maxString);
+	}
+
+	@Test
+	public void givenMaxNumberAt_1stPosition_Should_Return_SameNumber() {
+		Integer maxNumber = GenericsPrograms.testMaximum(50, 40, 30);
+		Assert.assertEquals(Integer.valueOf(50), maxNumber);
+	}
+
+	@Test
+	public void givenMaxNumberAt_2ndPosition_Should_Return_SameNumber() {
+		Integer maxNumber = GenericsPrograms.testMaximum(40, 50, 30);
+		Assert.assertEquals(Integer.valueOf(50), maxNumber);
+	}
+
+	@Test
+	public void givenMaxNumberAt_3rdPosition_Should_Return_SameNumber() {
+		Integer maxNumber = GenericsPrograms.testMaximum(40, 30, 50);
+		Assert.assertEquals(Integer.valueOf(50), maxNumber);
+	}
 
 }
